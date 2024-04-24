d5257101b3f362ae3abd6ddbee0c90b1f61624a7
Josh Polansky <josh@loupe.team>
2024-04-24 13:19:08 -0700

    Add docstring to AdsDriver

--- loupe.beckhoff_bridge/loupe/beckhoff_bridge/ads_driver.py
+++ loupe.beckhoff_bridge/loupe/beckhoff_bridge/ads_driver.py
@@ -1,15 +1,40 @@
 import pyads
 
 class AdsDriver():
+    """
+    A class that represents an ADS driver. It contains a list of variables to read from the target device and provides methods to read and write data.
+
+    Args:
+        ams_net_id (str): The AMS Net ID of the target device.
+
+    Attributes:
+        ams_net_id (str): The AMS Net ID of the target device.
+        _read_names (list): A list of names for reading data.
+        _read_struct_def (dict): A dictionary that maps names to structure definitions.
+
+    """
 
     def __init__(self, ams_net_id):             
+        """
+        Initializes an instance of the AdsDriver class.
 
+        Args:
+            ams_net_id (str): The AMS Net ID of the target device.
+
+        """
         self.ams_net_id = ams_net_id
         self._read_names = list()
         self._read_struct_def = dict()
 
     def add_read(self, name : str, structure_def = None):
+        """
+        Adds a variable to the list of data to read.
 
+        Args:
+            name (str): The name of the data to be read. "my_struct.my_array[0].my_var"
+            structure_def (optional): The structure definition of the data.
+
+        """
         if(name not in self._read_names):
             self._read_names.append(name)
 
@@ -18,10 +43,25 @@ class AdsDriver():
                 self._read_struct_def[name] = structure_def
 
     def write_data(self, data : dict ):
+        """
+        Writes data to the target device.
+
+        Args:
+            data (dict): A dictionary containing the data to be written. 
+            e.g.
+            data = {'MAIN.b_Execute': False, 'MAIN.str_TestString': 'Goodbye World', 'MAIN.r32_TestReal': 54.321}
+
+        """
         self._connection.write_list_by_name( data )
 
     def read_data(self):
-        # self._connection.
+        """
+        Reads all variables from the cyclic read list.
+
+        Returns:
+            dict: A dictionary containing the parsed data.
+
+        """
         if self._read_names.__len__() > 0:
             data = self._connection.read_list_by_name( self._read_names, structure_defs=self._read_struct_def)
             parsed_data = dict()
@@ -32,6 +72,20 @@ class AdsDriver():
         return parsed_data
         
     def _parse_name(self, name_dict, name, value):
+        """
+        Convert a variable from a flat name to a dictionary based structure.
+
+        "my_struct.my_array[0].my_var: value" -> {"my_struct": {"my_array": [{"my_var": value}]}}
+
+        Args:
+            name_dict (dict): The dictionary to store the parsed data.
+            name (str): The name of the data item.
+            value: The value of the data item.
+
+        Returns:
+            dict: The updated name_dict.
+
+        """
         name_parts = name.split(".")
         if len(name_parts) > 1:
             if name_parts[0] not in name_dict:
@@ -58,8 +112,18 @@ class AdsDriver():
                 name_dict[name_parts[0]] = value
         return name_dict
     
-    def connect(self):
+    def connect(self, ams_net_id = None):
+        """
+        Connects to the target device.
+
+        Args:
+            ams_net_id (str): The AMS Net ID of the target device. This does not need to be provided if it was provided in the constructor and has not changed.
+
+        """
+        if ams_net_id is not None:
+            self.ams_net_id = ams_net_id
+
         self._connection = pyads.Connection(self.ams_net_id, pyads.PORT_TC3PLC1)
-        self._connection.open()        
+        self._connection.open()
 
 
